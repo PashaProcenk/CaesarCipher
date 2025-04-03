b575b958a8e58593c405d4a5c40d1f20bcdae521
Pasha Procenk <pashaprocenk@icloud.com>
2025-04-04 00:50:18 +0300

    Ради интереса поменял Trows на блок try catch (Посмотрим что будет)

--- src/ServiceFile.java
+++ src/ServiceFile.java
@@ -7,8 +7,12 @@ public class ServiceFile {
     public static String readFile(String filePath) throws IOException {
         return new String(Files.readAllBytes(Paths.get(filePath)));
     }
-    public static void writeFile(String filePath, String content) throws IOException {
-        Files.write(Paths.get(filePath), content.getBytes());
+    public static void writeFile(String filePath, String content)  {
+        try {
+            Files.write(Paths.get(filePath), content.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
