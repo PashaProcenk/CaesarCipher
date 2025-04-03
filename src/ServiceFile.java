c610f411d4bb73d2eb05915b493d8fc11e1601b7
Pasha Procenk <pashaprocenk@icloud.com>
2025-04-03 23:44:18 +0300

    вторая попытка создания класса который читает файлы

--- src/ServiceFile.java
+++ src/ServiceFile.java
@@ -4,10 +4,10 @@ import java.nio.file.Paths;
 
 public class ServiceFile {
 
-    public  String readFile(String filePath) throws IOException {
+    public static String readFile(String filePath) throws IOException {
         return new String(Files.readAllBytes(Paths.get(filePath)));
     }
-    public void writeFile(String filePath, String content) throws IOException {
+    public static void writeFile(String filePath, String content) throws IOException {
         Files.write(Paths.get(filePath), content.getBytes());
 
     }
